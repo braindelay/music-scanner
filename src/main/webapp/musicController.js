f101f04171c0b03069f0617879066e4f57a21afb
Daniel Bray <daniel@braindelay.com>
2015-08-30 09:07:36 +0100

    updated some comments

--- src/main/webapp/musicController.js
+++ src/main/webapp/musicController.js
@@ -8,15 +8,14 @@ scanMusicControllers
         $scope.artists = data;
       });
     };
-
+    // get the current albums from the server
     $scope.getAlbums = function() {
           $http.get('/music/library/albums' , { cache : false } ).success(function(data) {
             $scope.albums = data;
           });
         };
 
-
-
+    // get the current songs from the server
     $scope.getSongs = function() {
           $http.get('/music/library/songs' , { cache : false } ).success(function(data) {
            $scope.songs = data;
@@ -30,29 +29,38 @@ scanMusicControllers
 
 
 
- $scope.searchSongs = function(item){
+  // the function used to filter the songs
+  // returns true if the artist/album set by "setFilter" matches the item
+  // if the album in setFilter is null, then the album part of the filter isn't applied
+   $scope.searchSongs = function(item){
     if(null == item || null == $scope.musicFilter || null == $scope.musicFilter.artist) {
       return true;
     } else {
-
       return item.artist == $scope.musicFilter.artist
-      && (null == $scope.musicFilter.album || item.album ==  $scope.musicFilter.album);
+        && (null == $scope.musicFilter.album || item.album ==  $scope.musicFilter.album);
     }
     return true;
- };
+  };
 
+  // the function used to render the song titles - returns the title string
   $scope.renderTitle = function(song) {
     return song.trackNumber +  "- " + song.title;
   }
+
+  // play the identified song
   $scope.play = function(song) {
 
+    // show the player
     $('#playerDiv').show();
+
+    //find the player, set the path to the song
+    // and then stop, reload, and play it
     var audio = $("#player");
-        $("#mp3_src").attr("src", "/music/download/" + song.songId);
-        /****************/
-        audio[0].pause();
-        audio[0].load();//suspends and restores all audio element
-        audio[0].play();
+    $("#mp3_src").attr("src", "/music/download/" + song.songId);
+    /****************/
+    audio[0].pause();
+    audio[0].load();//suspends and restores all audio element
+    audio[0].play();
   }
 
   }]);
